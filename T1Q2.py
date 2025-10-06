6c95a7700d8948aeb9d35bb2af6d2eff78178102
“acbd1324” <atulm@Atuls-MacBook-Pro.local>
2025-10-06 18:37:16 +0530

    First Task

--- T1Q2.py
+++ T1Q2.py
@@ -21,8 +21,9 @@ strings=[]
 no_of_strings = int(input("Enter the number of words required to sort : "))
 for i in range(no_of_strings):
     words = input("Enter word" + " " + str(i+1) + " : ")
-    strings.append(words)
-
+    strings.append(words.lower())
+    #because otherwise if any letter of the word is in a different case it will not search for it in Q3
+    #it also sorts capital first in the sort function as well so to remove that.3
 obj=Selection()
 sorted_list=obj.selection_sort(strings)
 print(sorted_list)
