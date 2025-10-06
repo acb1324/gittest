0030363c642c75990e98f2ee1bfc9c949cbd48bf
“acbd1324” <atulm@Atuls-MacBook-Pro.local>
2025-10-06 18:38:49 +0530

    First task

--- T1Q3.py
+++ T1Q3.py
@@ -12,14 +12,16 @@ class BinarySearcher:
                 high = mid - 1
         return -1
 
-from T1Q2 import Selection, sorted_list
+from T1Q2 import Selection , sorted_list
 
-item=input("Enter the word to be searched : ")
+item=input("Enter the word to be searched : ").lower()
 search=BinarySearcher()
 index=search.binary_search(sorted_list, item)
 
-if index == -1:
+if index == -1 :
     print("Sorry, the word is not present in the list")
 else:
     print("Your sorted list is "+ str(sorted_list) )
     print(item+" is found at index "+str(index))
+
+#it returns only lower case values to prevent the program from stopping if a different case word is given
